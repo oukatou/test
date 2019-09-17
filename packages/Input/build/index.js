5f5258c9fff71f4619255dbe4990a85c22374e2f
whd <530754967@qq.com>
2019-09-17 17:05:15 +0800

    update code

--- packages/Input/build/index.js
+++ packages/Input/build/index.js
@@ -7,7 +7,7 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var React = require('react');
 var React__default = _interopDefault(React);
 var emotion = require('emotion');
-var ControlBehavior = _interopDefault(require('whd-behavior/ControlBehavior'));
+var whdBehavior = require('whd-behavior');
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
@@ -40,7 +40,7 @@ var Input = function Input(props, ref) {
         otherProps = _objectWithoutProperties(props, ['placeholder', 'onChange', 'value']);
 
     return React__default.createElement(
-        ControlBehavior,
+        whdBehavior.ControlBehavior,
         null,
         function (_ref) {
             var hasHover = _ref.hasHover,
